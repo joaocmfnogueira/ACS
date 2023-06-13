57a8d8c71f1c2f724f166fa98d33b177c329439b
joaocmfnogueira <joao.nogueira@estudante.ufjf.br>
2023-06-13 18:14:09 -0300

    ...

--- 004_plot_pareto_front.py
+++ 004_plot_pareto_front.py
@@ -69,76 +69,76 @@ for (instance_name, results_name_list) in instances_results_name.items():
             problem_best_population = problem_best_population.reshape(problem_best_population.shape[0] * problem_best_population.shape[1], problem_best_population.shape[2])
             instances_results[instance_name][algorithm_name][i] = problem_best_population
 
-print('Generating graphs')
-print('=================\n')
+# print('Generating graphs')
+# print('=================\n')
 
-for (instance_name, algorithm_results) in instances_results.items():
-    print(instance_name)
-    print('---------\n')
-    for n in comparison_num_objectives:
-        print('%d objectives' % n)
-        num_students = len(algorithm_results['ga'])
-        for i in range(num_students):
-            problem_worst_point = worst_point[(instance_name, n)][i]
-            pareto_front = nondominated_population[(instance_name, n)][i]
-
-            # gd = get_performance_indicator("gd", pareto_front)
-
-            filepath = os.path.join(results_path, instance_name, '%d objectives' % n, '%d.%s' % (i, file_format))
-
-            legend = True if n <= 3 else False
-            scatter = Scatter(legend=legend)
-            scatter.add(pareto_front, label='Pareto-front', s=60, color=colors['pareto'])
-
-            size = 30
-            for algorithm_name in algorithm_results.keys():
-                problem_best_population = np.apply_along_axis(reduce_objectives, 1, algorithm_results[algorithm_name][i], n)
-                scatter.add(problem_best_population, label=algorithm_name, s=size, color=colors[algorithm_name])
-                size /= 2
-
-            scatter.add(problem_worst_point, label="Worst", s=100, color=colors['worst'])
-            scatter.save(filepath)
-
-
-
-
-# print('Comparing GA with NSGA-II using two objetives')
-# print('=============================================\n')
-# instances_results_name = create_results_name_list(instances, ['ga'], ['nsga_ii'], [2, 3])
-# for (instance_name, results_name_list) in instances_results_name.items():
+# for (instance_name, algorithm_results) in instances_results.items():
 #     print(instance_name)
 #     print('---------\n')
-#     for results_name_info in results_name_list:
-#         print(results_name_info)
-#         results_name = get_results_name(*results_name_info)
-#         results = open_results(results_name)
-#
-#         algorithm_name = results_name_info[0]
-#         num_objectives = results_name_info[2]
-#
-#         # (repetitions, students, individuals, num_objectives)
-#         best_population = get_results_best_n_objectives(results, 2)
-#         num_students = best_population.shape[1]
-#
+#     for n in comparison_num_objectives:
+#         print('%d objectives' % n)
+#         num_students = len(algorithm_results['ga'])
 #         for i in range(num_students):
-#             problem_best_population = best_population[:, i, :, :]
-#             problem_best_population = problem_best_population.reshape(problem_best_population.shape[0] * problem_best_population.shape[1], problem_best_population.shape[2])
-#
-#             pareto_front = nondominated_population[(instance_name, 2)][i]
-#             problem_worst_point = worst_point[(instance_name, 2)][i]
-#
-#             if num_objectives is None:
-#                 filepath = os.path.join(results_path, instance_name, '2 objectives', algorithm_name, '%d.%s' % (i, file_format))
-#             else:
-#                 filepath = os.path.join(results_path, instance_name, '2 objectives', '%s_%d' % (algorithm_name, num_objectives), '%d.%s' % (i, file_format))
-#
-#             scatter = Scatter(legend=True)
-#             scatter.add(pareto_front, label="Pareto-front")
-#             scatter.add(problem_best_population, label="Result", s=8)
-#             scatter.add(problem_worst_point, label="Worst", s=100)
+#             problem_worst_point = worst_point[(instance_name, n)][i]
+#             pareto_front = nondominated_population[(instance_name, n)][i]
+
+#             # gd = get_performance_indicator("gd", pareto_front)
+
+#             filepath = os.path.join(results_path, instance_name, '%d objectives' % n, '%d.%s' % (i, file_format))
+
+#             legend = True if n <= 3 else False
+#             scatter = Scatter(legend=legend)
+#             scatter.add(pareto_front, label='Pareto-front', s=60, color=colors['pareto'])
+
+#             size = 30
+#             for algorithm_name in algorithm_results.keys():
+#                 problem_best_population = np.apply_along_axis(reduce_objectives, 1, algorithm_results[algorithm_name][i], n)
+#                 scatter.add(problem_best_population, label=algorithm_name, s=size, color=colors[algorithm_name])
+#                 size /= 2
+
+#             scatter.add(problem_worst_point, label="Worst", s=100, color=colors['worst'])
 #             scatter.save(filepath)
-#     print('')
-#
+
+
+
+
+print('Comparing GA with NSGA-II using two objetives')
+print('=============================================\n')
+instances_results_name = create_results_name_list(instances, ['ga'], ['nsga_ii'], [2, 3])
+for (instance_name, results_name_list) in instances_results_name.items():
+    print(instance_name)
+    print('---------\n')
+    for results_name_info in results_name_list:
+        print(results_name_info)
+        results_name = get_results_name(*results_name_info)
+        results = open_results(results_name)
+
+        algorithm_name = results_name_info[0]
+        num_objectives = results_name_info[2]
+
+        # (repetitions, students, individuals, num_objectives)
+        best_population = get_results_best_n_objectives(results, 2)
+        num_students = best_population.shape[1]
+
+        for i in range(num_students):
+            problem_best_population = best_population[:, i, :, :]
+            problem_best_population = problem_best_population.reshape(problem_best_population.shape[0] * problem_best_population.shape[1], problem_best_population.shape[2])
+
+            pareto_front = nondominated_population[(instance_name, 2)][i]
+            problem_worst_point = worst_point[(instance_name, 2)][i]
+
+            if num_objectives is None:
+                filepath = os.path.join(results_path, instance_name, '2 objectives', algorithm_name, '%d.%s' % (i, file_format))
+            else:
+                filepath = os.path.join(results_path, instance_name, '2 objectives', '%s_%d' % (algorithm_name, num_objectives), '%d.%s' % (i, file_format))
+
+            scatter = Scatter(legend=True)
+            scatter.add(pareto_front, label="Pareto-front")
+            scatter.add(problem_best_population, label="Result", s=8)
+            scatter.add(problem_worst_point, label="Worst", s=100)
+            scatter.save(filepath)
+    print('')
+
 # assert(False)
 #
 # print('Comparing GA with NSGA-II using three objetives')
