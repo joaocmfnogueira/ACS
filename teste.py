48a1263268f04a4728e2417785917a83cbf43e0a
joaocmfnogueira <joao.nogueira@estudante.ufjf.br>
2023-07-09 20:49:35 -0300

    teset

---
+++ teste.py
@@ -0,0 +1,17 @@
+import numpy as np
+from pymoo.factory import get_problem
+from pymoo.visualization.scatter import Scatter
+from pymoo.factory import get_performance_indicator
+from pymoo.factory import get_performance_indicator
+
+# The pareto front of a scaled zdt1 problem
+pf = get_problem("zdt1").pareto_front()
+
+# The result found by an algorithm
+A = pf[::10] * 1.1
+
+igd = get_performance_indicator("igd", pf)
+print("IGD", igd.do(A))
+
+hv = get_performance_indicator("hv", ref_point=np.array([1.2, 1.2]))
+print("hv", hv.do(A))
