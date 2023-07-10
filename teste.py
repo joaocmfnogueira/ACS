4a86e268892ac2b8b819bb25206d1234c913b112
joaocmfnogueira <joao.nogueira@estudante.ufjf.br>
2023-07-09 21:15:26 -0300

    asdfasdf

--- teste.py
+++ teste.py
@@ -11,7 +11,9 @@ pf = get_problem("zdt1").pareto_front()
 a = pf[::10] * 1.1
 
 igd = get_performance_indicator("igd", pf)
-print("IGD", igd.do(a))
+valor1 = igd.do(a)
+print("IGD", valor1)
 
 hv = get_performance_indicator("hv", ref_point=np.array([1.2, 1.2]))
-print("hv", hv.do(a))
+valor2 = hv.do(a)
+print("hv", valor2)
